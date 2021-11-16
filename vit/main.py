1aca8d685a0a1b7ead90eb5b53e44f91ae578485
Aurel Feer <aurel.feer@gmail.com>
2021-11-16 17:28:13 +0100

    add training code

--- vit/main.py
+++ vit/main.py
@@ -1,57 +1,71 @@
 from transformers import ViTFeatureExtractor, ViTForImageClassification, ViTConfig
 from PIL import Image
-import requests
 
 from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import torch
 
+import torch.optim as optim
 
 def main():
 
 	transform = transforms.Compose([transforms.ToTensor(), transforms.Resize(224)])
+	feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+	model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+	for param in model.parameters():
+		param.requires_grad = False
+
+	model.classifier = torch.nn.Linear(in_features=768, out_features=10, bias=True)
+	for name, param in model.named_parameters():
+		print(name, param.size(), param.requires_grad)
+
+
+	train_dataset = CIFAR10("../datasets/", train=True, transform=transform)
+	train_dataloader = DataLoader(train_dataset, shuffle=False, batch_size=100, num_workers=2)
 
 	dataset = CIFAR10("../datasets/", train=False, transform=transform)
 	dataloader = DataLoader(dataset, shuffle=False, batch_size=100, num_workers=2)
 
-	url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-	image = Image.open(requests.get(url, stream=True).raw)
+	cifar_labels = ("airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
 
-	feature_extractor = ViTFeatureExtractor(size=32)
-	feature_extractor = feature_extractor.from_pretrained('google/vit-base-patch16-224')
-	
-	config = ViTConfig(image_size=32)
-	model = ViTForImageClassification(config=config)
-	model = model.from_pretrained('google/vit-base-patch16-224')
-	
-	cifar_labels = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+	loss_function = torch.nn.CrossEntropyLoss()
+	optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-	correct = 0
-	total = 0
+	model.train()
+	for epoch in range(1):
+		print(f"Epoch: {epoch}")
+		running_loss = 0.0
 
-	for images, labels in dataloader:
-		# print(cifar_labels[labels[0]])
-		image_list = [img for img in images]
+		for i, (images, labels) in enumerate(train_dataloader):
+			# print(cifar_labels[labels[0]])
 
-		inputs = feature_extractor(images=image_list, return_tensors="pt")
-		outputs = model(**inputs)
-		logits = outputs.logits
-		# model predicts one of the 1000 ImageNet classes
-		print(logits.shape)
+			# zero the parameter gradients
+			optimizer.zero_grad()
 
-		predicted_class_idx = logits.argmax(-1)
-		print(predicted_class_idx.shape)
-		for cls, label in zip(predicted_class_idx, labels):
-			for cifar_label in cifar_labels:
-				if cifar_label in model.config.id2label[cls.item()]:
-					if cifar_label == label:
-						correct += 1
-					# print("Predicted class:", cifar_label)
-					total += 1
-		if total >= 200:
-			break
+			image_list = [img for img in images]
 
-	print("percentage correct ", 100 * correct / total)
+			print("forward")
+			inputs = feature_extractor(images=image_list, return_tensors="pt")
+			outputs = model(**inputs)
+
+			print("backward")
+			loss = loss_function(outputs.logits, labels)
+			loss.backward()
+			print("step")
+			optimizer.step()
+
+			# print statistics
+			running_loss += loss.item()
+			if i % 10 == 9:    # print every 2000 mini-batches
+				print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 10))
+				running_loss = 0.0
+
+			logits = outputs.logits
+			# model predicts one of the 1000 ImageNet classes
+
+			predicted_class_idx = logits.argmax(-1)
 
 
 if __name__ == "__main__":
