99b101996aacb35f641378f4054968e0fd09501f
Martin Noruisis <martynas.nor@gmail.com>
2021-12-29 16:31:18 +0200

    Parse loss for ViT

--- parse_loss.py
+++ parse_loss.py
@@ -1,23 +1,45 @@
-import numpy as np
+def get_val_at_idx(line, idx):
+    return line.split()[idx].strip(',')
 
-## Resnet50
+objectives = [{
+                'name': 'resnet',
+                'input_file': 'resnet/resnet_results/scratch_resnet50.txt',
+                'output_folder': 'metrics/resnet',
+                'train_loss_key': 'train loss',
+                'train_loss_idx': 2,
+                'train_acc_idx': 4,
+                'validation_loss_key': 'validation loss',
+                'validation_loss_idx': 2,
+                'validation_acc_idx': 4
+              },
+              {
+                'name': 'vit',
+                'input_file': 'vit/vit_2021_12_09_run_scratch_mixed_prec.txt',
+                'output_folder': 'metrics/vit',
+                'train_loss_key': 'train_loss',
+                'train_loss_idx': 3,
+                'train_acc_idx': 5,
+                'validation_loss_key': 'eval_loss',
+                'validation_loss_idx': 3,
+                'validation_acc_idx': 5
+              },
+              
+              ]
 
-RESNET50_INPUT = 'resnet/resnet_results/scratch_resnet50.txt'
-RESNET50_OUTPUT = 'metrics/resnet'
-
-with open(RESNET50_INPUT, 'r') as resnet50_input, \
-    open(RESNET50_OUTPUT + '/train_loss.csv', 'w') as train_loss_file, \
-    open(RESNET50_OUTPUT + '/train_acc.csv', 'w') as train_acc_file, \
-    open(RESNET50_OUTPUT + '/validation_loss.csv', 'w') as validation_loss_file, \
-    open(RESNET50_OUTPUT + '/validation_acc.csv', 'w') as validation_acc_file:
-    for line in resnet50_input:
-        if 'train loss' in line:
-            train_loss = line[12:18]
-            train_loss_file.write(str(float(train_loss)) + '\n')
-            train_acc = line[25:31]
-            train_acc_file.write(str(float(train_acc)) + '\n')
-        if 'validation loss' in line:
-            validation_loss = line[17:23]
-            validation_loss_file.write(str(float(validation_loss)) + '\n')
-            validation_acc = line[30:36]
-            validation_acc_file.write(str(float(validation_acc)) + '\n')
+for objective in objectives:
+    with open(objective['input_file'], 'r') as input_file, \
+        open(objective['output_folder'] + '/train_loss.csv', 'w') as train_loss_file, \
+        open(objective['output_folder'] + '/train_acc.csv', 'w') as train_acc_file, \
+        open(objective['output_folder'] + '/validation_loss.csv', 'w') as validation_loss_file, \
+        open(objective['output_folder'] + '/validation_acc.csv', 'w') as validation_acc_file:
+        for line in input_file:
+            if objective['train_loss_key'] in line:
+                train_loss = get_val_at_idx(line, objective['train_loss_idx'])
+                train_loss_file.write(str(float(train_loss)) + '\n')
+                train_acc = get_val_at_idx(line, objective['train_acc_idx'])
+                train_acc_file.write(str(float(train_acc)) + '\n')
+            if objective['validation_loss_key'] in line:
+                validation_loss = get_val_at_idx(line, objective['validation_loss_idx'])
+                validation_loss_file.write(str(float(validation_loss)) + '\n')
+                validation_acc = get_val_at_idx(line, objective['validation_acc_idx'])
+                validation_acc_file.write(str(float(validation_acc)) + '\n')
