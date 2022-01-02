65cbc42800a36bdd2c2ca422a88052484d6904a6
Martin Noruisis <martynas.nor@gmail.com>
2022-01-02 13:04:13 +0200

    Increase text size in plots

--- metrics/plot_loss.py
+++ metrics/plot_loss.py
@@ -5,7 +5,8 @@ from loss import objectives
 
 
 TARGETS = ['train_loss', 'train_acc', 'validation_loss', 'validation_acc']
-TARGET_TITLES = ['Loss', 'Accuracy', 'Loss', 'Accuracy']
+TARGET_YLABELS = ['Loss', 'Accuracy', 'Loss', 'Accuracy']
+TARGET_TITLES = ['Training loss', 'Training accuracy', 'Test loss', 'Test accuracy']
 NO_OF_EPOCHS = 80
 COLOURS = ['red', 'green', 'blue']
 
@@ -15,29 +16,31 @@ def load_curve(input_file):
     return curve
 
 
-def plot_loss_curves(curves, labels, colors, output_path, name='loss_curve', title='Loss'):
+def plot_loss_curves(curves, labels, colors, output_path, name='loss_curve', ylabel='Loss', title='Loss'):
     fig, ax = plt.subplots(1, 1, figsize=(8, 4))
     
     for curve, label, color in zip(curves, labels, colors):
         ax.plot(range(len(curve)), curve, color=color, label=label)
 
+    ax.tick_params(axis='both', which='major', labelsize='x-large')
     ax.set_xlim(0, len(curve)-1)
-    ax.set_xlabel('Epoch', fontsize='large')
-    ax.set_ylabel(title, fontsize='large')
-    ax.legend(fontsize='large')
+    ax.set_xlabel('Epoch', fontsize='x-large')
+    ax.set_ylabel(ylabel, fontsize='x-large')
+    ax.legend(fontsize='x-large')
+    ax.set_title(title, fontsize='x-large')
 
     plt.savefig(output_path+'/'+name+'.pdf', dpi=1000, bbox_inches='tight')
 
 
 if __name__ == '__main__':    
     for objective in objectives:
-        for target, target_title in zip(TARGETS, TARGET_TITLES):
+        for target, target_ylabel, target_title in zip(TARGETS, TARGET_YLABELS, TARGET_TITLES):
             curve = load_curve(objective['output_folder']+'/'+target+'.csv')[:NO_OF_EPOCHS]
             plot_loss_curves([curve], [objective['name']], ['red'], \
-                objective['output_folder'], name=target, title=target_title)
+                objective['output_folder'], name=target, ylabel=target_ylabel, title=target_title)
     
-    for target, target_title in zip(TARGETS, TARGET_TITLES):
+    for target, target_ylabel, target_title in zip(TARGETS, TARGET_YLABELS, TARGET_TITLES):
         curves = [load_curve(objective['output_folder']+'/'+target+'.csv')[:NO_OF_EPOCHS] for objective in objectives]
         names = [objective['name'] for objective in objectives]
         plot_loss_curves(curves, names, COLOURS[:len(curves)], \
-            '.', name=target+'_combined', title=target_title)
+            '.', name=target+'_combined', ylabel=target_ylabel, title=target_title)
